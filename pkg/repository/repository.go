4075b86eae12301f81345a04ef5832d1bea2313b
wire1ess <putbusrff34h@mail.ru>
2024-07-13 20:04:42 +0300

    test commit

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,1 +1,20 @@
 package repository
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(authorization Authorization, todoList TodoList, todoItem TodoItem) *Repository {
+	return &Repository{Authorization: authorization, TodoList: todoList, TodoItem: todoItem}
+}
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,1 +1,20 @@
 package service
+
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(authorization Authorization, todoList TodoList, todoItem TodoItem) *Service {
+	return &Service{Authorization: authorization, TodoList: todoList, TodoItem: todoItem}
+}
